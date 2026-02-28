d21ecfd580c73de8323568370c49e2cd9790e0ac
James Eriksson <ja.mera@hotmail.com>
2026-03-01 00:37:32 +0100

    Added log

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,12 +4,18 @@ import { defineConfig, env } from "prisma/config"
 dotenv.config({ path: ".env" });
 dotenv.config({ path: ".env.local" })
 
+const databaseUrl = process.env.DATABASE_URL
+
+if (!databaseUrl) {
+  throw new Error("DATABASE_URL is not set in .env.local")
+}
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations"
   },
   datasource: {
-    url: process.env.DATABASE_URL!
+    url: databaseUrl
   }
 })

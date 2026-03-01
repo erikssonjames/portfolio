ee10974d0763c516efe81dffca5be230cc22c188
James Eriksson <ja.mera@hotmail.com>
2026-03-01 01:21:28 +0100

    Fix

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,6 @@
 import dotenv from  "dotenv";
 import { PrismaPg } from "@prisma/adapter-pg";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../generated/prisma";
 
 dotenv.config({ path: ".env" });
 dotenv.config({ path: ".env.local" });
